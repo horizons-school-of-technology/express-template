81ac5037f451227ac20a7afcecf2d45d00594db7
Carlie Ostrom <rlietrom@gmail.com>
2017-07-18 11:56:31 -0700

    nicde

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/messages', function(req, res) {
- res.send('received :fire:')
  var payload = JSON.parse(req.body.payload);  //lets us access the action they picked on the button
  console.log(payload)
  if(payload.actions[0].value === 'true') {
