6b4366257ab1606dd977b81876b1c2872ff3b09a
bubbadaboss77 <rfeldman@bates.edu>
2017-07-19 15:43:10 -0700

    ok

--- cron.js
+++ cron.js
@@ -1,12 +1,16 @@
-console.log('CRON RUNNING')
-
-var {User} = require('./models')
-var {web} = require ('./bot')
+console.log('hellow im running')
+//npm install cron
+//heroku run npm run cron
+//to choose frequency, go to heroku and add scheduling in add-ons
+//add new job
+//npm run cron
+var { User } = require('/models');
+var { web } = require('bot')
 
 User.findOne()
-.then(function(user) {
-  web.chat.postMessage(user.slackDmId, 'Current time is ' + new Date(),
-  function() {
-    process.exit(0)
-  })
-})
+    .then(function(user){
+        web.chat.postMessage(user.slackDmId, 'currrent time is ' + new Date(),
+        function(){
+            process.exit(0); // this ends the script
+        })
+    })
