d9a382d61976a297c8b50673b216f7874d03362b
Julia <jklugher@wellesley.edu>
2017-08-01 16:55:25 -0700

    change titles to bookswap

--- models.js
+++ models.js
@@ -3,13 +3,25 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
   username: String,
   password: String,
+  email: String,
   phone: String
 });
-
-
-
 User = mongoose.model('User', userSchema);
 
+const bookSchema = mongoose.Schema({
+    owner: {
+        ref: User,
+        type: mongoose.Schema.Types.ObjectId
+    },
+    title: String,
+    author: String,
+    department: String,
+    price: Number
+
+});
+const Book = mongoose.model('Book', bookSchema);
+
 module.exports = {
-    User:User
+    User: User,
+    Book: Book
 };
