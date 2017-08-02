3a754801728180d19fef788971da070c185e47ec
Julia <jklugher@wellesley.edu>
2017-08-02 15:09:15 -0700

    fixing search on home

--- routes/routes.js
+++ routes/routes.js
@@ -9,10 +9,7 @@ var depts = require('../departments');
 // Users who are not logged in can see these routes
 
 router.get('/', function(req, res, next) {
-  console.log(depts.depts);
-  res.render('home', {
-    depts: depts.depts
-  });
+  res.render('home');
 });
 
 ///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
